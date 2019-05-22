ce608934ba4422c38815df53cc0d866a10c2919d
Matt Condon <mattgcondon@gmail.com>
2019-05-22 14:28:27 -0700

    fix: remove prettier formatting

--- packages/aws-appsync/src/cache/offline-cache.ts
+++ packages/aws-appsync/src/cache/offline-cache.ts
@@ -103,11 +103,11 @@ export default class MyCache extends InMemoryCache {
     super.write(write);
 
     if (!this.storeCacheRootMutation && write.dataId === "ROOT_MUTATION") {
-      // @ts-ignore private accessor
+        // @ts-ignore private access
       this.data.delete("ROOT_MUTATION");
     }
 
-    // @ts-ignore private accessor
+    // @ts-ignore private access
     if (this.data && typeof (this.data as any).record === "undefined") {
       // do not persist contents of a RecordingCache
       const data = super.extract(true);
